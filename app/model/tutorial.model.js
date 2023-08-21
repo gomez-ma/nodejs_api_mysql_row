d65fe6135a8e36091e124b3e44a3476916b76273
gomez-ma <gomes.ma@northbkk.ac.th>
2023-08-21 15:37:42 +0700

    first commit

---
+++ app/model/tutorial.model.js
@@ -0,0 +1,20 @@
+const sql = require("./db.js");
+
+const Tutorial = function(tutorial){
+    this.title = tutorial.title;
+    this.description = tutorial.description;
+    this.published = tutorial.published;
+};
+
+Tutorial.getAll = (title, result) => {
+    let sqltext = "SELECT * FROM tutorials";
+
+    sql.query(sqltext, (err, res)=>{
+        if(err){
+            console.log("error: ", err)
+            return;
+        }
+
+        console.log("tutorials: ", res);
+    });
+};
